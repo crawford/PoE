274d2b7da8626f42372df54d5c77a0ed38dbf0e9
Alex Crawford <poe@accounts.acrawford.com>
2018-02-20 15:11:11 -0800

    firmware/main: remove led example

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -26,7 +26,6 @@ fn main() {
     let cmu = peripherals.CMU;
     let gpio = peripherals.GPIO;
     let msc = peripherals.MSC;
-    let timer = peripherals.TIMER0;
     let mut nvic = efm32gg11b820::CorePeripherals::take().unwrap().NVIC;
 
     // Enable the HFXO
@@ -50,9 +49,8 @@ fn main() {
     let _ = cmu.status.read().bits();
 
     cmu.hfbusclken0.write(|reg| reg.gpio().set_bit());
-    cmu.hfperclken0.write(|reg| reg.timer0().set_bit());
-
-    gpio.ph_dout.reset();
+    gpio.ph_dout
+        .write(|reg| unsafe { reg.dout().bits(0x3F << 10) });
     gpio.ph_modeh.write(|reg| {
         reg.mode10().wiredand();
         reg.mode11().wiredand();
@@ -63,65 +61,10 @@ fn main() {
         reg
     });
 
-    timer.cmd.write(|reg| reg.stop().set_bit());
-    timer.cnt.reset();
-    timer.ctrl.write(|reg| {
-        reg.presc().div1024();
-        reg.clksel().preschfperclk();
-        reg.falla().none();
-        reg.risea().none();
-        reg.mode().up();
-        reg.debugrun().clear_bit();
-        reg.dmaclract().clear_bit();
-        reg.qdm().clear_bit();
-        reg.osmen().clear_bit();
-        reg.x2cnt().clear_bit();
-        reg.ati().clear_bit();
-        reg
-    });
-    timer
-        .top
-        .write(|reg| unsafe { reg.top().bits(50_000_000 / (1024 * 10)) });
-    timer.ifc.write(|reg| {
-        reg.of().set_bit();
-        reg.uf().set_bit();
-        reg.dirchg().set_bit();
-        reg.cc0().set_bit();
-        reg.cc1().set_bit();
-        reg.cc2().set_bit();
-        reg.cc3().set_bit();
-        reg.icbof0().set_bit();
-        reg.icbof1().set_bit();
-        reg.icbof2().set_bit();
-        reg.icbof3().set_bit();
-        reg
-    });
-    timer.ien.write(|reg| reg.of().set_bit());
-
-    nvic.clear_pending(efm32gg11b820::Interrupt::TIMER0);
-    nvic.enable(efm32gg11b820::Interrupt::TIMER0);
-
-    timer.cmd.write(|reg| reg.start().set_bit());
-
     loop {
         asm::wfe();
     }
 }
-
-fn isr_timer0() {
-    interrupt::free(|_| unsafe {
-        (*efm32gg11b820::TIMER0::ptr())
-            .ifc
-            .write(|reg| reg.of().set_bit());
-        (*efm32gg11b820::GPIO::ptr()).ph_dout.modify(|read, write| {
-            write
-                .dout()
-                .bits((((read.dout().bits() >> 10) + 0x0B) & 0x3F) << 10)
-        });
-    })
-}
-
-interrupt!(TIMER0, isr_timer0);
 
 // Light up both LEDs yellow, trigger a breakpoint, and loop
 #[lang = "panic_fmt"]
