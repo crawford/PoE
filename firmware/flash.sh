592f19a39b127542ba275cb24844b32cd2382e2b
Alex Crawford <poe@code.acrawford.com>
2018-03-25 12:53:41 -0700

    firmware/build: update rust and nix derivation
    This is a little more ergonomic than runCommand and bumps to Rust
    Nightly from 2018-04-16. This also no longer depends on a local overlay
    and removes the need for Xargo.

--- firmware/flash.sh
+++ firmware/flash.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # XXX: Use a vanilla build of OpenOCD at some point
-~/code/crawford/openocd/src/openocd \
-	--search ~/code/crawford/openocd/tcl \
+~/code/openocd/src/openocd \
+	--search ~/code/openocd/tcl \
 	--file openocd.cfg \
 	--command "program $1 verify" \
 	--command "init" \
