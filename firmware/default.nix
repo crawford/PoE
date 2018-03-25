b3aceb37762932c5ebaac5802727506926bc0a07
Alex Crawford <poe@accounts.acrawford.com>
2018-03-25 12:53:41 -0700

    firmware/build: update rust and nix derivation
    This is a little more ergonomic than runCommand and bumps to Rust
    Nightly from 2018-04-16. This also no longer depends on a local overlay
    and removes the need for Xargo.

--- firmware/default.nix
+++ firmware/default.nix
@@ -1,6 +1,18 @@
-with import <nixos> {};
-runCommand "dummy" {
-  buildInputs = [
-    ((rustChannelOf{ date = "2018-02-19"; channel = "nightly"; }).rust.override{ extensions = [ "rust-src" ]; })
-  ];
-} ""
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+in
+  with nixpkgs;
+  stdenv.mkDerivation {
+    name = "poe";
+    buildInputs = [
+      ((rustChannelOf {
+        date = "2018-04-16";
+        channel = "nightly";
+      }).rust.override {
+        targets = [ "thumbv7m-none-eabi" ];
+        extensions = [ "rust-std" "rustfmt-preview" ];
+      })
+      gcc-arm-embedded
+    ];
+  }
