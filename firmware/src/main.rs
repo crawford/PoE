566fe08c9d5c0a081476086ca90cce73f20e9079
Alex Crawford <poe@code.acrawford.com>
2021-07-12 09:09:49 -0700

    main: migrate to rtic

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -15,11 +15,10 @@
 #![no_main]
 #![no_std]
 
-extern crate panic_itm;
-
 mod efm32gg;
 mod ksz8091;
 mod mac;
+mod network;
 mod phy;
 
 use crate::efm32gg::dma;
@@ -27,155 +26,221 @@ use crate::ksz8091::KSZ8091;
 use core::fmt::Write;
 use core::pin::Pin;
 use cortex_m::{asm, peripheral};
-use efm32gg11b820::interrupt;
 use efm32gg_hal::cmu::CMUExt;
 use efm32gg_hal::gpio::{pins, EFM32Pin, GPIOExt, Output};
+use ignore_result::Ignore;
 use led::rgb::{self, Color};
 use led::LED;
-use smoltcp::iface::{InterfaceBuilder, NeighborCache};
-use smoltcp::socket::{SocketSet, TcpSocket, TcpSocketBuffer};
+use panic_itm as _;
+use rtic::cyccnt::U32Ext;
+use smoltcp::iface::{InterfaceBuilder, Neighbor, NeighborCache};
+use smoltcp::socket::{SocketSet, SocketSetItem, TcpSocket, TcpSocketBuffer};
 use smoltcp::time::Instant;
-use smoltcp::wire::{EthernetAddress, IpAddress, IpCidr};
+use smoltcp::wire::{EthernetAddress, IpAddress, IpCidr, Ipv4Address, Ipv4Cidr};
 
-#[cortex_m_rt::entry]
-fn main() -> ! {
-    let mut rx_region = dma::RxRegion([0; 1536]);
-    let mut tx_region = dma::TxRegion([0; 1536]);
-    let mut rx_descriptors = dma::RxDescriptors::new();
-    let mut tx_descriptors = dma::TxDescriptors::new();
-    let rx_buffer = dma::RxBuffer::new(Pin::new(&mut rx_region), Pin::new(&mut rx_descriptors));
-    let tx_buffer = dma::TxBuffer::new(Pin::new(&mut tx_region), Pin::new(&mut tx_descriptors));
+type LED0 = rgb::CommonAnodeLED<pins::PH10<Output>, pins::PH11<Output>, pins::PH12<Output>>;
+type LED1 = rgb::CommonAnodeLED<pins::PH13<Output>, pins::PH14<Output>, pins::PH15<Output>>;
 
-    let peripherals = efm32gg11b820::Peripherals::take().unwrap();
-    let cmu = peripherals.CMU;
-    let eth = peripherals.ETH;
-    let gpio = peripherals.GPIO;
-    let msc = peripherals.MSC;
-    let rtc = peripherals.RTC;
+#[cfg(feature = "logging")]
+type Logger = cortex_m_log::log::Logger<cortex_m_log::printer::itm::InterruptSync>;
 
-    // Enable the HFXO
-    cmu.oscencmd.write(|reg| reg.hfxoen().set_bit());
-    // Wait for HFX0 to stabilize
-    while cmu.status.read().hfxordy().bit_is_clear() {}
+#[rtic::app(device = efm32gg11b820, peripherals = true, monotonic = rtic::cyccnt::CYCCNT)]
+const APP: () = {
+    struct Resources {
+        led0: LED0,
+        led1: LED1,
+        #[cfg(feature = "logging")]
+        logger: Logger,
+        network: network::Resources,
+        rtc: efm32gg11b820::RTC,
+    }
 
-    // Update the EMU configuration
-    let _ = cmu.status.read().bits();
+    #[init]
+    fn init(mut cx: init::Context) -> init::LateResources {
+        static mut ETH_RX_REGION: dma::RxRegion = dma::RxRegion([0; 1536]);
+        static mut ETH_TX_REGION: dma::TxRegion = dma::TxRegion([0; 1536]);
+        static mut ETH_RX_DESCRIPTORS: dma::RxDescriptors = dma::RxDescriptors::new();
+        static mut ETH_TX_DESCRIPTORS: dma::TxDescriptors = dma::TxDescriptors::new();
+        static mut TCP_RX_PAYLOAD: [u8; 128] = [0; 128];
+        static mut TCP_TX_PAYLOAD: [u8; 128] = [0; 128];
 
-    // Allow access to low energy peripherals with a clock speed greater than 50MHz
-    cmu.ctrl.write(|reg| reg.wshfle().set_bit());
+        static mut NEIGHBORS: [Option<(IpAddress, Neighbor)>; 8] = [None; 8];
+        static mut SOCKETS: [Option<SocketSetItem<'static>>; 1] = [None; 1];
+        static mut IP_ADDRESSES: [IpCidr; 1] = [IpCidr::Ipv4(Ipv4Cidr::new(
+            Ipv4Address::new(192, 168, 0, 3),
+            24,
+        ))];
 
-    // Set the appropriate read delay for flash
-    msc.readctrl.write(|reg| reg.mode().ws2());
+        // Enable the HFXO
+        cx.device.CMU.oscencmd.write(|reg| reg.hfxoen().set_bit());
+        // Wait for HFX0 to stabilize
+        while cx.device.CMU.status.read().hfxordy().bit_is_clear() {}
 
-    // Switch to high frequency oscillator
-    cmu.hfclksel.write(|reg| reg.hf().hfxo());
+        // Update the EMU configuration
+        let _ = cx.device.CMU.status.read().bits();
 
-    // Use the high frequency clock for the ITM
-    cmu.dbgclksel.write(|reg| reg.dbg().hfclk());
+        // Allow access to low energy peripherals with a clock speed greater than 50MHz
+        cx.device.CMU.ctrl.write(|reg| reg.wshfle().set_bit());
 
-    // Update the EMU configuration
-    let _ = cmu.status.read().bits();
+        // Set the appropriate read delay for flash
+        cx.device.MSC.readctrl.write(|reg| reg.mode().ws2());
 
-    // Enable the GPIOs and the low energy peripheral interface
-    cmu.hfbusclken0.write(|reg| {
-        reg.gpio().set_bit();
-        reg.le().set_bit();
-        reg
-    });
+        // Switch to high frequency oscillator
+        cx.device.CMU.hfclksel.write(|reg| reg.hf().hfxo());
 
-    // Enable the RTC and set it to 1000Hz
-    cmu.lfaclksel.write(|reg| reg.lfa().ulfrco());
-    cmu.lfaclken0.write(|reg| reg.rtc().set_bit());
-    rtc.ctrl.write(|reg| reg.en().set_bit());
+        // Use the high frequency clock for the ITM
+        cx.device.CMU.dbgclksel.write(|reg| reg.dbg().hfclk());
 
-    gpio.ph_dout
-        .write(|reg| unsafe { reg.dout().bits(0x3F << 10) });
-    gpio.ph_modeh.write(|reg| {
-        reg.mode10().wiredand();
-        reg.mode11().wiredand();
-        reg.mode12().wiredand();
-        reg.mode13().wiredand();
-        reg.mode14().wiredand();
-        reg.mode15().wiredand();
-        reg
-    });
+        // Update the EMU configuration
+        let _ = cx.device.CMU.status.read().bits();
 
-    gpio.routepen.write(|reg| reg.swvpen().set_bit());
-    gpio.routeloc0.write(|reg| reg.swvloc().loc0());
-    gpio.pf_model.write(|reg| reg.mode2().pushpull());
+        // Enable the GPIOs and the low energy peripheral interface
+        cx.device.CMU.hfbusclken0.write(|reg| {
+            // TODO: Gets set by cx.device.GPIO.split(), but is needed because of the write to routepen before.
+            reg.gpio().set_bit();
+            reg.le().set_bit();
+            reg
+        });
 
-    #[cfg(feature = "logging")]
-    let _logger = {
-        use cortex_m_log::destination::Itm;
-        use cortex_m_log::log::Logger;
-        use cortex_m_log::printer::itm::InterruptSync;
+        // Enable the Serial Wire Viewer (ITM on SWO)
+        cx.device.GPIO.routepen.write(|reg| reg.swvpen().set_bit());
 
-        let itm = cortex_m::peripheral::Peripherals::take().unwrap().ITM;
+        // Enable the RTC and set it to 1000Hz
+        cx.device.CMU.lfaclksel.write(|reg| reg.lfa().ulfrco());
+        cx.device.CMU.lfaclken0.write(|reg| reg.rtc().set_bit());
+        cx.device.RTC.ctrl.write(|reg| reg.en().set_bit());
 
-        let logger = Logger::<InterruptSync> {
-            inner: InterruptSync::new(Itm::new(itm)),
-            level: log::LevelFilter::Trace,
-        };
-        unsafe { cortex_m_log::log::trick_init(&logger) }.unwrap();
-        log::debug!("Logger online!");
+        // Enable the DWT (needed by monotonic timer)
+        cx.core.DWT.enable_cycle_counter();
+        cx.core.DCB.enable_trace();
 
-        logger
-    };
+        let gpio = cx.device.GPIO.split(cx.device.CMU.constrain().split().gpio);
+        let _swo = gpio.pf2.as_output();
 
-    let ethernet_addr = EthernetAddress([0x02, 0x00, 0x00, 0x00, 0x00, 0x02]);
-    let mut neighbor_cache = [None; 8];
-    let mut ip_addrs = [IpCidr::new(IpAddress::v4(10, 1, 0, 3), 24)];
+        let mut led0 = rgb::CommonAnodeLED::new(
+            gpio.ph10.as_opendrain(),
+            gpio.ph11.as_opendrain(),
+            gpio.ph12.as_opendrain(),
+        );
 
-    let mut iface = InterfaceBuilder::new(
-        efm32gg::EFM32GG::new(rx_buffer, tx_buffer, eth, &cmu, &gpio, KSZ8091::new)
+        let mut led1 = rgb::CommonAnodeLED::new(
+            gpio.ph13.as_opendrain(),
+            gpio.ph14.as_opendrain(),
+            gpio.ph15.as_opendrain(),
+        );
+
+        led0.set(Color::Black);
+        led1.set(Color::Black);
+
+        let interface = InterfaceBuilder::new(
+            efm32gg::EFM32GG::new(
+                dma::RxBuffer::new(Pin::new(ETH_RX_REGION), Pin::new(ETH_RX_DESCRIPTORS)),
+                dma::TxBuffer::new(Pin::new(ETH_TX_REGION), Pin::new(ETH_TX_DESCRIPTORS)),
+                cx.device.ETH,
+                &cx.device.CMU,
+                &cx.device.GPIO,
+                KSZ8091::new,
+            )
             .expect("unable to create MACPHY"),
-    )
-    .ethernet_addr(ethernet_addr)
-    .neighbor_cache(NeighborCache::new(neighbor_cache.as_mut()))
-    .ip_addrs(ip_addrs.as_mut())
-    .finalize();
+        )
+        .ethernet_addr(EthernetAddress([0x02, 0x00, 0x00, 0x00, 0x00, 0x02]))
+        .neighbor_cache(NeighborCache::new(NEIGHBORS.as_mut()))
+        .ip_addrs(IP_ADDRESSES.as_mut())
+        .finalize();
 
-    let mut tcp_rx_payload = [0; 128];
-    let mut tcp_tx_payload = [0; 128];
-    let tcp_socket = TcpSocket::new(
-        TcpSocketBuffer::new(tcp_rx_payload.as_mut()),
-        TcpSocketBuffer::new(tcp_tx_payload.as_mut()),
-    );
+        #[cfg(feature = "logging")]
+        let logger = {
+            use cortex_m_log::destination::Itm;
+            use cortex_m_log::log::trick_init;
+            use cortex_m_log::printer::itm::InterruptSync;
 
-    let mut socket_array = [None];
-    let mut sockets = SocketSet::new(socket_array.as_mut());
-    let tcp_handle = sockets.add(tcp_socket);
+            let logger = Logger {
+                inner: InterruptSync::new(Itm::new(cx.core.ITM)),
+                level: log::LevelFilter::Debug,
+            };
 
-    loop {
-        log::trace!("WFE");
-        asm::wfe();
+            unsafe { trick_init(&logger) }.unwrap();
+            log::debug!("Logger online!");
 
-        let timestamp = Instant::from_millis(rtc.cnt.read().cnt().bits());
-        match iface.poll(&mut sockets, timestamp) {
-            Ok(_) => {}
-            Err(err) => log::error!("Failed to poll: {}", err),
-        }
+            logger
+        };
 
-        {
-            let mut socket = sockets.get::<TcpSocket>(tcp_handle);
-            if !socket.is_open() {
-                socket.listen(6969).unwrap();
-            }
+        let mut sockets = SocketSet::new(SOCKETS.as_mut());
+        let tcp_handle = sockets.add(TcpSocket::new(
+            TcpSocketBuffer::new(TCP_RX_PAYLOAD.as_mut()),
+            TcpSocketBuffer::new(TCP_TX_PAYLOAD.as_mut()),
+        ));
 
-            if socket.can_send() {
-                log::debug!("tcp:6969 send greeting");
-                writeln!(socket, "hello").unwrap();
-                log::debug!("tcp:6969 close");
-                socket.close();
-            }
+        init::LateResources {
+            led0,
+            led1,
+            #[cfg(feature = "logging")]
+            logger,
+            network: network::Resources {
+                interface,
+                sockets,
+                tcp_handle,
+            },
+            rtc: cx.device.RTC,
         }
     }
-}
 
-#[efm32gg11b820::interrupt]
-fn ETH() {
-    efm32gg::isr()
-}
+    #[task(resources = [network, rtc], schedule = [handle_network])]
+    fn handle_network(cx: handle_network::Context) {
+        log::trace!("Handling network...");
+
+        let network::Resources {
+            interface,
+            sockets,
+            tcp_handle,
+        } = cx.resources.network;
+
+        let timestamp = Instant::from_millis(cx.resources.rtc.cnt.read().cnt().bits());
+        match interface.poll(sockets, timestamp) {
+            Ok(false) => log::trace!("Nothing to do"),
+            Ok(true) => {
+                log::trace!("Handling sockets...");
+
+                let mut socket = sockets.get::<TcpSocket>(*tcp_handle);
+                if !socket.is_open() {
+                    socket.listen(6969).unwrap();
+                }
+
+                if socket.can_send() {
+                    log::debug!("tcp:6969 send greeting");
+                    writeln!(socket, "hello").unwrap();
+                    log::debug!("tcp:6969 close");
+                    socket.close();
+                }
+            }
+            Err(err) => log::error!("Failed to poll network interface: {}", err),
+        }
+
+        if let Some(delay) = interface.poll_delay(sockets, timestamp) {
+            cx.schedule
+                // TODO: 80_000 cycles shouldn't be hard-coded (I'm not sure if it's right either)
+                .handle_network(cx.scheduled + (delay.millis as u32 * 80_000).cycles())
+                .ignore();
+
+            log::trace!("Scheduled network handling in {}", delay);
+        }
+        log::trace!("Finished handling network");
+    }
+
+    #[task(binds = ETH, resources = [led0], spawn = [handle_network])]
+    fn eth_irq(cx: eth_irq::Context) {
+        log::trace!("Interrupt - ETH");
+
+        efm32gg::isr();
+
+        cx.spawn.handle_network().ignore();
+    }
+
+    extern "C" {
+        fn CAN0();
+        fn CAN1();
+    }
+};
 
 // Light up both LEDs red, trigger a breakpoint, and loop
 #[cortex_m_rt::exception]
@@ -213,9 +278,6 @@ fn HardFault(_frame: &cortex_m_rt::ExceptionFrame) -> ! {
         asm::wfe();
     }
 }
-
-type LED0 = rgb::CommonAnodeLED<pins::PH10<Output>, pins::PH11<Output>, pins::PH12<Output>>;
-type LED1 = rgb::CommonAnodeLED<pins::PH13<Output>, pins::PH14<Output>, pins::PH15<Output>>;
 
 unsafe fn steal_leds() -> (LED0, LED1) {
     let gpio = (&*efm32gg11b820::GPIO::ptr())
