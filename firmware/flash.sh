d379f00bc571379086a26db4a61a5f42ea9404d6
Alex Crawford <poe@code.acrawford.com>
2022-01-17 00:58:55 -0800

    build: add --remain flag to flash.sh
    This allows OpenOCD to remain running after flashing the target, which
    is helpful if itmdump is following the ITM log in the background. The
    workflow is then to simply invoke `cargo run` to build, flash, and run
    the firmware and then allow the background itmdump to stream the logs.

--- firmware/flash.sh
+++ firmware/flash.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i sh -p openocd
 
+post_program="exit"
+
+if [[ "${1}" == "--remain" ]]
+then
+    shift
+    post_program=""
+fi
+
 openocd \
 	--file openocd.cfg \
-	--command "program $1 verify reset exit"
+	--command "program ${1} verify reset ${post_program}"
