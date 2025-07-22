10974a8824eb027647d039b6bbb3ddbbe54e01bd
Alex Crawford <poe@code.acrawford.com>
2025-07-22 15:29:19 -0700

    Fix the logger so that it doesn't use UB

--- firmware/src/log/mod.rs
+++ firmware/src/log/mod.rs
@@ -13,28 +13,19 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use core::mem::MaybeUninit;
+use core::cell::UnsafeCell;
 
 pub mod itm;
 pub mod rtt;
 
-static mut LOGGER: MaybeUninit<Logger> = MaybeUninit::uninit();
+static LOGGER: Logger = Logger::new();
 
 pub fn init() -> InitializedLogger {
     static mut INITIALIZED: bool = false;
     assert!(unsafe { !INITIALIZED }, "logger already initialized");
     unsafe { INITIALIZED = true };
 
-    log::set_logger(unsafe {
-        LOGGER.write(Logger {
-            #[cfg(feature = "itm")]
-            itm: None,
-
-            #[cfg(feature = "rtt")]
-            rtt: None,
-        })
-    })
-    .expect("set_logger");
+    log::set_logger(&LOGGER).expect("set_logger");
 
     InitializedLogger {}
 }
@@ -46,7 +37,7 @@ impl InitializedLogger {
     #[cfg(feature = "itm")]
     pub fn add_itm(&self, logger: itm::Logger) -> &Self {
         log::set_max_level(log::max_level().max(logger.level));
-        unsafe { LOGGER.assume_init_mut().itm = Some(logger) };
+        unsafe { *LOGGER.itm.get() = Some(logger) };
 
         log::info!("ITM logging online!");
         self
@@ -55,7 +46,7 @@ impl InitializedLogger {
     #[cfg(feature = "rtt")]
     pub fn add_rtt(&self, logger: rtt::Logger) -> &Self {
         log::set_max_level(log::max_level().max(logger.level));
-        unsafe { LOGGER.assume_init_mut().rtt = Some(logger) };
+        unsafe { *LOGGER.rtt.get() = Some(logger) };
 
         log::info!("RTT logging online!");
         self
@@ -64,22 +55,33 @@ impl InitializedLogger {
 
 struct Logger {
     #[cfg(feature = "itm")]
-    itm: Option<itm::Logger>,
+    itm: UnsafeCell<Option<itm::Logger>>,
 
     #[cfg(feature = "rtt")]
-    rtt: Option<rtt::Logger>,
+    rtt: UnsafeCell<Option<rtt::Logger>>,
+}
+
+unsafe impl Sync for Logger {}
+
+impl Logger {
+    const fn new() -> Self {
+        Logger {
+            itm: UnsafeCell::new(None),
+            rtt: UnsafeCell::new(None),
+        }
+    }
 }
 
 impl log::Log for Logger {
     fn enabled(&self, metadata: &log::Metadata) -> bool {
         #[cfg(feature = "itm")]
-        match &self.itm {
+        match unsafe { &*self.itm.get() } {
             Some(itm) if itm.enabled(metadata) => return true,
             _ => {}
         }
 
         #[cfg(feature = "rtt")]
-        match &self.rtt {
+        match unsafe { &*self.rtt.get() } {
             Some(rtt) if rtt.enabled(metadata) => return true,
             _ => {}
         }
@@ -89,24 +91,24 @@ impl log::Log for Logger {
 
     fn log(&self, record: &log::Record) {
         #[cfg(feature = "itm")]
-        if let Some(itm) = &self.itm {
+        if let Some(itm) = unsafe { &*self.itm.get() } {
             itm.log(record);
         }
 
         #[cfg(feature = "rtt")]
-        if let Some(rtt) = &self.rtt {
+        if let Some(rtt) = unsafe { &*self.rtt.get() } {
             rtt.log(record);
         }
     }
 
     fn flush(&self) {
         #[cfg(feature = "itm")]
-        if let Some(itm) = &self.itm {
+        if let Some(itm) = unsafe { &*self.itm.get() } {
             itm.flush();
         }
 
         #[cfg(feature = "rtt")]
-        if let Some(rtt) = &self.rtt {
+        if let Some(rtt) = unsafe { &*self.rtt.get() } {
             rtt.flush();
         }
     }
